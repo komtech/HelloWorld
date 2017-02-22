d952ff73ec92a13c292470990ad6eeb82d03e758
Sopheara Seng <soseng@live.com>
2017-02-22 11:34:58 -0800

    remove comment

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Hello World using GitHub");
 		System.out.println("Modification Message");
 		System.out.println("branch bug #1234");
