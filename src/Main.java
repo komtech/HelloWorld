85dff3df12f5bc7eb88164f55ea6dbdfe21f6688
komtech <soseng@live.com>
2017-02-21 23:58:33 -0800

    Update Main.java
    remote update and pull back to local repository

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
 		System.out.println("Modification Message");
 		System.out.println("branch bug #1234");
 		System.out.println("Another commit and push to remote github!!");
+		System.out.println("made change in remote and pull back to local repository!!");
 	}
 
 }
